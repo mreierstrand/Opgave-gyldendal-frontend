f2035bde2e32296fe4b8fcbee7621aee93268b0e
Oliver Eierstrand <48084927+mreierstrand@users.noreply.github.com>
2025-04-10 04:03:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // For deployment til main-grenen, brug root som base
+  base: '/Opgave-gyldendal-frontend/', // For deployment til main-grenen, brug root som base
   plugins: [react()],
 });
